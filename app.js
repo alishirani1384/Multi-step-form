b5394c2c9c0151fbc456d0953b8d5e4174c9ae98
Ali Shirani <alishiranii1384@gmail.com>
2023-01-08 14:12:42 +0330

    FIX

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 const infoInputs = document.querySelectorAll(".step-1 form input");
-const nextBtn = document.querySelector(".next-stp");
-let currentStep = 1;
-const step = document.querySelector(`.step-${currentStep}`);
-
-
+const nextBtns = document.querySelectorAll(".next-stp");
+const prevBtns = document.querySelectorAll(".prev-stp");
+let currentStep = 0;
+const step = document.querySelectorAll(".stp");
+shownextStep(currentStep)
+function shownextStep(n) {
+    step[n].style.display = "flex"; 
+    if (n > 0) {
+      step[n-1].style.display = "none";  
+    }
+}
+function showPrevStep(n) {
+    if(n>0)step[n].style.display = "flex";
+    
+}
 infoInputs.forEach(input => {
-    nextBtn.addEventListener("click", () => {
+    nextBtns.forEach(nextBtn => {
+        nextBtn.addEventListener("click", () => {
         if (!input.value) {
             findLabel(input).nextElementSibling.style.display = "flex";
             input.classList.add("err");
-        } else {  
+        }else {  
             findLabel(input).nextElementSibling.style.display = "none";
             input.classList.remove("err")
         }
+        if (validateForm() == true) {
+            shownextStep(currentStep+1)
+        }
+    })
     })
 
 })
 
+function validateForm() {
+    let valid = true;
+    for (let i = 0; i < infoInputs.length; i++) {
+        if (!infoInputs[i].value) {
+            valid = false;
+        } else {
+            valid=true
+        }    
+    }
+    return valid;
+}
 function findLabel(el) {
     const idVal = el.id;
     const labels = document.getElementsByTagName("label");
@@ -24,3 +50,8 @@ function findLabel(el) {
       if (labels[i].htmlFor == idVal) return labels[i];
     }
 }
+
+
+prevBtns.forEach(prevBtn => {
+    prevBtn.addEventListener("click",showPrevStep(currentStep-1))
+})
